086e8f96de9b91f63cac2d1e082173ff0612501f
Loganliotta <loganliotta03@gmail.com>
2026-04-30 15:23:30 +0900

    B: gemini intergrated

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise RuntimeError("API key not found.")
-client = genai.Client(api_key=api_key)
-print(client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.").text)
-
-
-
-
-
+from google.genai import types
+import argparse
 
 def main():
-    print("Hello from ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("API key not found.")
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    #print(args.user_prompt)
+    #print(response.text)
+
+    if args.verbose is True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+
+
+
 
 
 if __name__ == "__main__":
